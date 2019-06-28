7051bcd267085b9486adc13c757f5aabb840858b
rsmsnot <rsmsnot@outlook.com>
2019-06-28 02:35:40 +0000

    solved recusrively

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-  let sum = 0;
-  // if (fromN === toN) {
-  //   return sum += fromN;;
-  // }
-
-  
-  for (let i = fromN; i < (toN + 1); i++) {
-  sum += i;
+  let total = 0;
+  if (fromN === toN) {
+    return total += toN;;
   }
-  return sum;
-
+  return fromN + sum(fromN + 1, toN);
 }
 
 module.exports = sum; 
