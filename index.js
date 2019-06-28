604777804c2e6f12d431c4d38d563d74068e1b88
rsmsnot <rsmsnot@outlook.com>
2019-06-28 02:28:57 +0000

    sum from a to b

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let sum = 0;
+  // if (fromN === toN) {
+  //   return sum += fromN;;
+  // }
+
+  
+  for (let i = fromN; i < (toN + 1); i++) {
+  sum += i;
+  }
+  return sum;
+
 }
 
-module.exports = sum;
+module.exports = sum; 
